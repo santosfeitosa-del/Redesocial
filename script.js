3bbc3f855b0a1743e321c1d50c6cac504f89c8ed
santosfeitosa-del <santos.feitosa@escola.pr.gov.br>
2026-08-06 12:14:12 -0300

    ok

---
+++ script.js
@@ -0,0 +1,8 @@
+//Botão curtida
+
+document.addEventListener("DOMcontentLoaded",() => {
+const likeBtn = document.querySelector(".left-actions .action-btn:first-child");
+if (!likeBtn) return;
+const likeSvg = likeBtn.querySelector("svg");
+    
+} 
