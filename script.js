7a62a14a05efd3ca67a877c96d3fa054534bc06d
santosfeitosa-del <santos.feitosa@escola.pr.gov.br>
2026-08-13 12:16:20 -0300

    ok

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 //Botão curtida
-
 document.addEventListener("DOMcontentLoaded",() => {
 const likeBtn = document.querySelector(".left-actions .action-btn:first-child");
 if (!likeBtn) return;
 const likeSvg = likeBtn.querySelector("svg");
-    
-} 
+
+//localiza o contador
+
+let textNode = Array.from(likeBtn.childNodes).find(node) => node.nodeType
+}) 
