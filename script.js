d5aaae681c84522aa0d2d45fe59496abe2d36dec
santosfeitosa-del <santos.feitosa@escola.pr.gov.br>
2026-08-20 12:00:52 -0300

    ok

--- script.js
+++ script.js
@@ -7,4 +7,13 @@ const likeSvg = likeBtn.querySelector("svg");
 //localiza o contador
 
 let textNode = Array.from(likeBtn.childNodes).find(node) => node.nodeType
+=== Node.TEXT_NODE && node.textContent.trim() !== ""
 }) 
+
+//zera o contador
+let cont = 0;
+
+//atualiza
+if(textNode){
+    textNode.textContent = '0';
+}
